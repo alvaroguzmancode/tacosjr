2f58bad96df26bb78ab5fb595df19c7229887b23
Alvaro Guzman Rodriguez <aguzman@acsyt.com>
2025-12-09 15:44:27 -0600

    fix

--- src/components/LocationMap.tsx
+++ src/components/LocationMap.tsx
@@ -18,7 +18,7 @@ export default function LocationMap() {
             </div>
 
             <a
-                href="https://www.google.com/maps/place/Tacos+J%26R/@25.7107968,-100.3009077,14z/data=!4m10!1m2!2m1!1sTacos+J%26R!3m6!1s0x866295a4bd660a1f:0xfd272075dce5f0da!8m2!3d25.7107968!4d-100.2627989!15sCglUYWNvcyBKJlKSAQpyZXN0YXVyYW504AEA!16s%2Fg%2F11yls0pbgj?entry=ttu&g_ep=EgoyMDI1MTIwMi4wIKXMDSoASAFQAw%3D%3D"
+                href="https://www.google.com/maps/place/Tacos+J%26R/@25.710798,-100.2640153,19z/data=!3m1!4b1!4m7!3m6!1s0x866295a4bd660a1f:0xfd272075dce5f0da!8m2!3d25.7107968!4d-100.2627989!15sCglUYWNvcyBKJlKSAQpyZXN0YXVyYW504AEA!16s%2Fg%2F11yls0pbgj?entry=tts&g_ep=EgoyMDI1MTIwMi4wIPu8ASoASAFQAw%3D%3D&skid=3301bf84-48ce-42b0-be22-4fb5b497b6eb"
                 target="_blank"
                 rel="noreferrer"
                 className="mt-4 inline-flex items-center justify-center gap-2 w-full bg-blue-600 hover:bg-blue-700 text-white font-bold px-4 py-3 rounded-xl transition shadow-lg"
