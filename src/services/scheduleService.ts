3979b93224f542fe0ea1a679997536a278616cd1
Alvaro Guzman Rodriguez <aguzman@acsyt.com>
2025-12-12 15:53:56 -0600

    fix Sábado cerrado

--- src/services/scheduleService.ts
+++ src/services/scheduleService.ts
@@ -6,7 +6,7 @@ export const schedule: ScheduleDay[] = [
     { day: "Miércoles", open: "7:00 a. m.", close: "2:00 p. m." },
     { day: "Jueves", open: "7:00 a. m.", close: "2:00 p. m." },
     { day: "Viernes", open: "7:00 a. m.", close: "2:00 p. m." },
-    { day: "Sábado", open: "7:00 a. m.", close: "12:30 p. m." },
+    { day: "Sábado", open: null, close: null },
     { day: "Domingo", open: null, close: null }
 ];
 
