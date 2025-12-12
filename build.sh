c95c3e35a5eca7e2a64ad5430b6ddf33cee85b85
Alvaro Guzman Rodriguez <aguzman@acsyt.com>
2025-12-12 16:02:55 -0600

    fix build command

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-ssh -i /Users/alvaro/Documents/projects/vps/vps/vps/ovh_rsa.pem ubuntu@148.113.180.49 '
+sudo ssh -i /Users/alvaro/Documents/projects/vps/vps/vps/ovh_rsa.pem ubuntu@148.113.180.49 '
 cd /home/ubuntu/projects/tacosjr/tacosjr &&
 git pull &&
-(npx pm2 delete tacosjr || true) &&
-yarn build &&
-npx pm2 start "serve -s dist -l 3002" --name tacosjr
+(/home/ubuntu/.nvm/versions/node/v20.18.1/bin/npx pm2 delete tacosjr || true) &&
+/home/ubuntu/.nvm/versions/node/v20.18.1/bin/yarn build &&
+/home/ubuntu/.nvm/versions/node/v20.18.1/bin/npx pm2 start "serve -s dist -l 3002" --name tacosjr
 '
